1497f222e8eae1ea49197c222ae7b717ba1f58c1
Anawarul Haque <anujgaonkar11@gmail.com>
2023-11-23 18:18:30 +0530

    tested docker image of postgres

---
+++ src/main/java/com/neosoft/dockerdbtest/DockerDbTestApplication.java
@@ -0,0 +1,13 @@
+package com.neosoft.dockerdbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDbTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neosoft/dockerdbtest/DockerDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.dockerdbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
